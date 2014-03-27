dbccf5200d48678f68f9cb417519e867d78f413d
lstemp1234 <lstemp1234@kangaroobox.com>
2014-03-27 15:04:40 -0400

    initial Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Challenge::Application.config.secret_key_base = '10f22ab1bc576f1d351eb3f98ed72b4b747a546ec47f0a043ee578ad5a4022c29d7f951353bcc37577cbdb7a80aed1476887cf8de7a1d3f3741d67f6474e9848'
